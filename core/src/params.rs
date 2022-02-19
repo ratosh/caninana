86f487793ed0c09cab65cc6b8f0b3b70ff1fee01
Raoni Campos <ratosh@gmail.com>
2022-02-19 23:30:26 +0100

    Roach burrowing behavior

--- core/src/params.rs
+++ core/src/params.rs
@@ -5,6 +5,8 @@ pub const UNLOCK_ROACH_WORKERS: usize = 25;
 pub const UNLOCK_HYDRA_WORKERS: usize = 45;
 pub const UNLOCK_LATE_TECH_WORKERS: usize = 54;
 pub const UNLOCK_OVERSEER_WORKERS: usize = 29;
+pub const UNLOCK_BURROW_WORKERS: usize = 26;
+pub const OVERLORD_SPEED_WORKERS: usize = 30;
 
 pub const CREEP_DISTANCE_TO_HALL: f32 = 5f32;
 pub const CREEP_SPREAD_MAP_DISTANCE: usize = 4;
@@ -14,3 +16,6 @@ pub const CREEP_SPREAD_ENERGY: u32 = 125;
 pub const TRANSFUSION_MIN_ENERGY: u32 = 50;
 pub const TRANSFUSION_MAX_RANGE: f32 = 15f32;
 pub const TRANSFUSION_MISSING_HEALTH: u32 = 60;
+
+pub const BURROW_HEALTH_PERCENTAGE: f32 = 0.55f32;
+pub const UNBURROW_HEALTH_PERCENTAGE: f32 = 0.9f32;
