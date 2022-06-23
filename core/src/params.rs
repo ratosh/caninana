f17d8da4af6ab80120141ac2c30f47921c49183f
Raoni Campos <ratosh@gmail.com>
2022-06-23 22:33:54 +0100

    Improving queen behavior (#108)

--- core/src/params.rs
+++ core/src/params.rs
@@ -1,5 +1,5 @@
 pub const MAX_WORKERS: usize = 80;
-pub const MAX_QUEENS: usize = 8;
+pub const MAX_QUEENS: usize = 10;
 
 pub const MULTI_EVOLUTION_WORKERS: usize = 60;
 pub const DOUBLE_GAS_PER_BASE_WORKERS: usize = 38;
@@ -15,17 +15,22 @@ pub const UNLOCK_UPGRADES_WORKERS: usize = 40;
 pub const SAVE_FOR_ATTACK_UPGRADES_ON_UNITS: usize = 4;
 pub const SAVE_FOR_DEFENSE_UPGRADES_ON_UNITS: usize = 8;
 
-pub const CREEP_DISTANCE_TO_HALL: f32 = 3.5f32;
+pub const CREEP_DISTANCE_TO_HALL: f32 = 4.5f32;
 pub const CREEP_SPREAD_MAP_DISTANCE: usize = 5;
-pub const CREEP_SPREAD_DISTANCE: usize = 6;
-pub const CREEP_SPREAD_ENERGY: u32 = 55;
+pub const CREEP_SPREAD_DISTANCE: usize = 5;
+pub const CREEP_SPREAD_ENERGY_MIN: u32 = 40;
+pub const CREEP_SPREAD_ENERGY: u32 = 180;
+pub const CREEP_SPREAD_ENERGY_PER_TUMOR: u32 = 1;
 
 pub const TRANSFUSION_MIN_ENERGY: u32 = 50;
 pub const TRANSFUSION_MAX_RANGE: f32 = 15f32;
-pub const TRANSFUSION_MISSING_HEALTH: u32 = 60;
+pub const TRANSFUSION_MISSING_HEALTH: u32 = 50;
 
+pub const INJECTION_MAX_LARVA_PER_BASE: usize = 4;
 pub const INJECTION_MAX_LARVA: usize = 30;
 
 pub const BURROW_HEALTH_PERCENTAGE: f32 = 0.58f32;
 pub const BURROW_DETECTION_RANGE: f32 = 9f32;
 pub const UNBURROW_HEALTH_PERCENTAGE: f32 = 0.95f32;
+
+pub const QUEEN_INJECT_SWITCH_BASE_RANGE: f32 = 3f32;
